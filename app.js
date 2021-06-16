9c832e2bb4c0c265a48f0ff6515d722108422d01
rj13-to <you@example.com>
2021-06-16 17:19:36 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var express =require("express");
+var app=express();
+var mongoose=require("mongoose");
+var bodyParser =require("body-parser");
+var path=require("path");
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect("mongodb://localhost/bookOcean",{ useNewUrlParser: true , useUnifiedTopology: true })
+.then(()=> console.log("data base connection succesfull!!! "))
+.catch((err)=> console.log(err));
+app.use(express.static("public"));
+// post routes ends
+app.get("/",function(req,res){
+    res.render("home.ejs");
+})
+app.get("*",function(req,res){
+    res.render("ushome.ejs");
+})
+
+app.listen(3000,function(){
+    console.log("server started!!!");
+})
